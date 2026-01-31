037df7dd5424ab3f4e8fbbeb9a0c41cbb5635c95
Sean Collins <yubtubfromtheyubtub@gmail.com>
2026-01-31 17:03:18 -0500

    create base game logic and event loop

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include "fwwasm.h"
+#include <stdint.h>
+
+int character;
+int level;
+int steps;
+int litLeds;
+
+void createNewSave(){
+    //ask user to pick a character
+}
+
+void eventLoop(){
+    uint8_t event_data[FW_GET_EVENT_DATA_MAX] = {0};
+    
+    int last_event;
+    if(hasEvent()){
+        last_event = getEventData(event_data);
+    }
+
+    if(last_event == FWGUI_EVENT_GUI_SENSOR_DATA){
+        //process sensor data
+    }
+}
+
+WASM_EXPORT int main(){
+    bool saved = fileExists("data.txt");
+    if(!saved){
+        createNewSave();
+    }else{
+        static char buff[256];
+        int handle = openFile("data.txt", 0);
+
+        int bytesRead = readFile(handle, (unsigned char*)buff, sizeof(buff) - 1);
+        buff[bytesRead] = '\0';
+
+        character = (int)buff[0] - (int)'0';
+        level = (int)buff[1] - (int)'0';
+        litLeds = (int)buff[2] - (int)'0';
+
+        switch(level){
+            case 1:
+                steps = (int)((100 / 7) * litLeds);
+                break;
+            case 2:
+                steps = (int)((200 / 7) * litLeds);
+                break;
+            case 3:
+                steps = (int)((300 / 7) * litLeds);
+                break;
+            default:
+                break;
+        }
+    }
+
+    while(1){
+        eventLoop();
+    }
+
+    return 0;
+}
