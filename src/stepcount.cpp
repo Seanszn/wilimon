a2d9b622377434e194df479b72de9f2577d5c263
Sean Collins <yubtubfromtheyubtub@gmail.com>
2026-01-31 18:20:13 -0500

    make src folder

---
+++ src/stepcount.cpp
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+void processAccelData(uint8_t* event_data, bool* up, int* steps){
+    int16_t iY;
+    bool curr_up;
+
+    iY = static_cast<int16_t>(event_data[2] | event_data[3] << 8);
+    (iY > 0) ? curr_up = 1 : curr_up = 0;
+    
+    if(curr_up != *up){ (*steps)++; }
+}
