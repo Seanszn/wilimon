7f3923f61b5ee04a6398d1e456efe7dc3be1931e
pix-xl <jmarquezfebles@oakland.edu>
2026-01-31 23:05:35 -0500

    readable colors

--- src/battle.cpp
+++ src/battle.cpp
@@ -1,5 +1,15 @@
 #include "fwwasm.h"
 
+#define WHITE 0x999999
+#define YELLOW 0x999900
+#define GREEN 0x009900
+#define BLUE 0x000099
+#define RED 0x990000
+
+#define GET_R(x) ((x >> 16) & 0xFF)
+#define GET_G(x) ((x >> 8) & 0xFF)
+#define GET_B(x) (x & 0xFF)
+
 const int battleTime = 30000;
 const int switchTime = 6000;
 
@@ -8,11 +18,29 @@ const float lvTwo = 1.1;
 const float lvThree = 1.2;
 const float lvFour = 1.4;
 
-// const int WHITE = 0;
-// const int YELLOW = 1;
-// const int GREEN = 2;
-// const int BLUE = 3;
-// const int RED = 5;
+void setAllLEDS(int color) {
+    for (int i = 0; i < 7; i++) {
+        setBoardLED(i, 
+            GET_R(color), 
+            GET_G(color), 
+            GET_B(color), 
+            switchTime, 
+            ledsimplevalue);
+    }
+}
+
+
+bool evaluateBattle(float value){
+
+}
+
+void displayVictory(int* character){
+
+}
+
+void displayLoss(int* character){
+
+}
 
 void beginBattle(int* level, int* character, int* steps){
     int mult;
@@ -44,23 +72,23 @@ void beginBattle(int* level, int* character, int* steps){
         switch(color){
             //WHITE
             case 0:
-                setAllLEDS(255, 255, 255);
+                setAllLEDS(WHITE);
                 break;
             //YELLOW
             case 1:
-                setAllLEDS(255, 255, 0);
+                setAllLEDS(YELLOW);
                 break;
             //GREEN
             case 2:
-                setAllLEDS(0, 255, 0);
+                setAllLEDS(GREEN);
                 break;
             //BLUE
             case 3:
-                setAllLEDS(0, 0, 255);
+                setAllLEDS(BLUE);
                 break;
             //RED
             case 4:
-                setAllLEDS(255, 0, 0);
+                setAllLEDS(RED);
                 break;
             default:
                 break;
@@ -79,23 +107,5 @@ void beginBattle(int* level, int* character, int* steps){
         displayVictory(character);
     }else{
         displayLoss(character);
-    }
-}
-
-bool evaluateBattle(float value){
-
-}
-
-void displayVictory(int* character){
-
-}
-
-void displayLoss(int* character){
-
-}
-
-void setAllLEDS(int red, int green, int blue) {
-    for (int i = 0; i < 7; i++) {
-        setBoardLED(i, red, green, blue, switchTime, ledsimplevalue);
     }
 }
