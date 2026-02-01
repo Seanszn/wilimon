df0a9eeb5fdfcd9de21bb2ef9f37a52b7c0eb7b8
Sean Collins <yubtubfromtheyubtub@gmail.com>
2026-01-31 22:49:59 -0500

    added color switch case

--- src/battle.cpp
+++ src/battle.cpp
@@ -8,11 +8,11 @@ const float lvTwo = 1.1;
 const float lvThree = 1.2;
 const float lvFour = 1.4;
 
-const int WHITE = 0;
-const int YELLOW = 1;
-const int GREEN = 2;
-const int BLUE = 3;
-const int RED = 5;
+// const int WHITE = 0;
+// const int YELLOW = 1;
+// const int GREEN = 2;
+// const int BLUE = 3;
+// const int RED = 5;
 
 void beginBattle(int* level, int* character, int* steps){
     int mult;
@@ -36,7 +36,40 @@ void beginBattle(int* level, int* character, int* steps){
     }
 
     int startTime = millis();
-    int flashTime = millis();
+    int flashTime = startTime;
+
+    while(millis() - startTime < battleTime){
+        int color = wilirand() + startTime % 5;
+        
+        switch(color){
+            //WHITE
+            case 0:
+                setAllLEDS(255, 255, 255);
+                break;
+            //YELLOW
+            case 1:
+                setAllLEDS(255, 255, 0);
+                break;
+            //GREEN
+            case 2:
+                setAllLEDS(0, 255, 0);
+                break;
+            //BLUE
+            case 3:
+                setAllLEDS(0, 0, 255);
+                break;
+            //RED
+            case 4:
+                setAllLEDS(255, 0, 0);
+                break;
+            default:
+                break;
+        }
+
+        while(millis() - flashTime){
+
+        }
+    }
 
     float points = clicks * mult;
     bool won = evaluateBattle(points);
